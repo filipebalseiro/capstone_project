2645d5c2240f3092f4a4bc45e049777bf4f6c6f8
filipebalseiro <filipe_balseiro@hakkoda.io>
2023-11-28 16:37:52 +0000

    capstone project

---
+++ models/intermediate/int_claim_diagnosis_pivoted.sql
@@ -0,0 +1,20 @@
+with 
+
+claim_diagnoses as (
+    select * from {{ ref('stg_claim_diagnosis') }}
+),
+
+final as (
+
+    select 
+        claim_id, 
+        {{ pivot_claim_diagnoses(3) }}
+    
+    from claim_diagnoses
+
+    group by 1
+
+
+)
+
+select * from final
